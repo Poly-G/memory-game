3cf0777e248f97c032ba92b07cd647931575feb0
Poly Goveia <poly.goveia@gmail.com>
2018-05-31 17:59:40 -0700

    cards that match will stay in place, 2 cards open at a time, but there is a bug if you click fast enough

--- js/app.js
+++ js/app.js
@@ -1,27 +1,28 @@
 /*
-Tutorial provided by Mike Wales via https://www.youtube.com/watch?reload=9&reload=9&v=_rUH-sEs68Y&app=desktop
-*/
+ * Tutorial provided by Mike Wales via https://www.youtube.com/watch?reload=9&reload=9&v=_rUH-sEs68Y&app=desktop
+ */
 
 /*
  * Create a list that holds all of your cards
  */
 
-const cards = [ 'fa-diamond', 'fa-diamond',
-                'fa-paper-plane-o', 'fa-paper-plane-o',
-                'fa-anchor', 'fa-anchor',
-                'fa-bolt', 'fa-bolt',
-                'fa-cube', 'fa-cube',
-                'fa-leaf', 'fa-leaf',
-                'fa-bicycle', 'fa-bicycle',
-                'fa-bomb', 'fa-bomb'
-              ]
+ /*
+  * Display the cards on the page
+  *   - shuffle the list of cards using the provided "shuffle" method below
+  *   - loop through each card and create its HTML
+  *   - add each card's HTML to the page
+  */
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+  /*
+   * set up the event listener for a card. If a card is clicked:
+   *  - display the card's symbol (put this functionality in another function that you call from this one)
+   *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+   *  - if the list already has another card, check to see if the two cards match
+   *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+   *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+   *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+   *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+   */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -39,32 +40,29 @@ function shuffle(array) {
   return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+const cards = [ 'fa-diamond', 'fa-diamond',
+                'fa-paper-plane-o', 'fa-paper-plane-o',
+                'fa-anchor', 'fa-anchor',
+                'fa-bolt', 'fa-bolt',
+                'fa-cube', 'fa-cube',
+                'fa-leaf', 'fa-leaf',
+                'fa-bicycle', 'fa-bicycle',
+                'fa-bomb', 'fa-bomb'
+              ]
 
 function generateCard(card) {
-  return `<li class="card" datacard="${card}"><i class="fa ${card}"></i></li>`;
+return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
 
-function startGame() {
-  var deck = document.querySelector('.deck');
-  var cardHTML = shuffle(cards).map(function(card) {
+function initGame() {
+  let deck = document.querySelector('.deck');
+  let cardHTML = shuffle(cards).map(function(card) {
     return generateCard(card);
   });
   deck.innerHTML = cardHTML.join('');
 }
 
-
-
-startGame();
+initGame();
 
 let moves = document.querySelector('.moves');
 let actualMoves = 0;
@@ -75,32 +73,35 @@ function showCard(e) {
   e.classList.add('open', 'show');
 }
 
+
 allCards.forEach(function(card) {
   card.addEventListener('click', function(e) {
 
-    if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+    if(!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
       openCards.push(card);
       showCard(card);
 
-      //check if they mathc
-      // const firstCardType = openCards[0].dataset.card;
-      // console.log(firstCardType);
+
 
       if (openCards.length == 2) {
         if (openCards[0].dataset.card == openCards[1].dataset.card) {
+            openCards[0].classList.add('match');
             openCards[0].classList.add('open');
             openCards[0].classList.add('show');
 
+            openCards[1].classList.add('match');
             openCards[1].classList.add('open');
             openCards[1].classList.add('show');
             openCards = [];
-          } else {
-              setTimeout(function() {
-                openCards.forEach(function(card) {
-                  card.ClassList.remove('open', 'show');
-                });
-                openCards = [];
-              }, 1000);
+        } else {
+            // if cards dont match - go away
+            setTimeout(function(){
+            openCards.forEach(function(card){
+              card.classList.remove('open', 'show');
+            });
+
+            openCards = [];
+          }, 1000);
         }
 
         actualMoves ++;
@@ -108,6 +109,7 @@ allCards.forEach(function(card) {
       }
     }
   });
+
 });
 
 
@@ -120,4 +122,41 @@ allCards.forEach(function(card) {
 
 
 
-//hi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//makemelifeeasier

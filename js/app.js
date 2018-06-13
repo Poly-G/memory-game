562fdc17377dea51277d3b9e9de309c546bdc699
Poly Goveia <poly.goveia@gmail.com>
2018-06-13 10:01:02 -0700

    start timer when page is loaded

--- js/app.js
+++ js/app.js
@@ -2,27 +2,8 @@
  * Tutorial provided by Mike Wales via https://www.youtube.com/watch?reload=9&reload=9&v=_rUH-sEs68Y&app=desktop
  */
 
-/*
- * Create a list that holds all of your cards
- */
 
- /*
-  * Display the cards on the page
-  *   ^ shuffle the list of cards using the provided "shuffle" method below
-  *   ^ loop through each card and create its HTML
-  *   ^ add each card's HTML to the page
-  */
 
-  /*
-   * set up the event listener for a card. If a card is clicked:
-   *  - display the card's symbol (put this functionality in another function that you call from this one)
-   *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-   *  - if the list already has another card, check to see if the two cards match
-   *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-   *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-   *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-   *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-   */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -65,15 +46,19 @@ function generateCard(card) {
 return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
 
+//stack overflow (https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa)
+const minutes = document.querySelector("#minutes")
+    const seconds = document.querySelector("#seconds")
+    let count = 0;
 
-// start timer
-function startTimer (){
-  let time = 0;
-  let timer = setInterval(function(){
-    time++;
-    console.log(time);
-  }, 1000);
-};
+    const renderTimer = () => {
+      count += 1;
+      minutes.innerHTML = Math.floor(count / 60).toString().padStart(2, "0");
+      seconds.innerHTML = (count % 60).toString().padStart(2, "0");
+    }
+
+    const timer = setInterval(renderTimer, 1000)
+
 
 // clear timer
 function clearTimer (){
@@ -86,7 +71,6 @@ function initGame() {
   let cardHTML = shuffle(cards).map(function(card) {
     return generateCard(card);
   });
-  
   deck.innerHTML = cardHTML.join('');
   
 }
@@ -143,9 +127,7 @@ allCards.forEach(function(card) {
             openCards = [];
           }, 700);
         }
-        startTimer ()
         removeStars();
-        console.log(actualMoves);
         actualMoves ++;
         moves.innerHTML = `<span class="moves">${actualMoves}</span>`;
       }
@@ -154,3 +136,16 @@ allCards.forEach(function(card) {
 
 });
 
+
+
+
+  /*
+   * set up the event listener for a card. If a card is clicked:
+   *  - display the card's symbol (put this functionality in another function that you call from this one)
+   *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+   *  - if the list already has another card, check to see if the two cards match
+   *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+   *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+   *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+   *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+   */
